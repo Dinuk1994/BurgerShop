603826f15c0938868119086e10fd25a4bde1c4ff
Dinuk1994 <ddt941119@gmail.com>
2023-12-22 18:13:40 +0530

    JS forms

--- Js/pastaAdd.js
+++ Js/pastaAdd.js
@@ -5,24 +5,21 @@ let dis=null;
 let date=null;
 let qty=null;
 
-
 function add(){
     code=document.getElementById("txtCode").value;
-    iName=document.getElementById("txtName").value;
+    iName=documert.getElementById("txtName").value;
     price=document.getElementById("txtPrice").value;
     dis=document.getElementById("txtDis").value;
     date=document.getElementById("txtExp").value;
     qty=document.getElementById("txtQty").value;
 
-    let itemList=JSON.parse(localStorage.getItem("friesItem"))||[];
+    let itemList=JSON.parse(localStorage.getItem("pastaItem"))|| [];
     itemList.push({code,iName,price,dis,date,qty});
 
-    localStorage.setItem("friesItem",JSON.stringify(itemList));
+    localStorage.setItem("pastaItem",JSON.stringify(itemList));
 
     updateTable();
 
     return false;
+
 }
-
-
-
